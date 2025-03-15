fee88cbd31c91f32a6e2487068dedb4471607d4c
Amer <amerlopo5@gmail.com>
2025-03-15 17:51:02 +0100

    Mobile responsive

--- src/components/scoreCard.tsx
+++ src/components/scoreCard.tsx
@@ -15,20 +15,22 @@ const ScoreCard: React.FC<scoreCardProps> = ({
 }) => {
   return (
     <div
-      className={`${className} flex items-center justify-between p-3 rounded-lg shadow-[0px_0px_25px_5px_rgba(0,_0,_0,_0.1)] w-1/2 bg-white`}
+      className={`${className} flex items-center justify-between p-3 rounded-lg shadow-[0px_0px_25px_5px_rgba(0,_0,_0,_0.1)] md:w-1/2 w-6/10 bg-white`}
     >
       <div className="flex items-center">
-        <span className="text-black font-bold ml-2.5 box-border">
+        <span className="text-black font-bold ml-2.5 box-border  text-[12px] md:text-[1rem]">
           #{placement}
         </span>
         <img
-          className="w-8 h-8 ml-4 my- rounded-full"
+          className="w-6 h-6 md:w-8 md:h-8 ml-4 rounded-full"
           src={image}
           alt="Profile"
         />
-        <p className="ml-4">{name}</p>
+        <p className="ml-4 text-[12px] md:text-[1rem]">{name}</p>
       </div>
-      <p className="text-right mr-2.5 tracking-wider">{points} Bodova</p>
+      <p className="text-right mr-2.5 tracking-wider  text-[12px] md:text-[1rem]">
+        {points} Bodova
+      </p>
     </div>
   );
 };
--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -60,19 +60,21 @@ const Leaderboard: React.FC = () => {
           backgroundImage: `url(${backgroundImage})`,
           backgroundSize: "cover",
         }}
-        className="w-[70%] aspect-[60/43] py-6 pt-20 overflow-hidden"
+        className="w-[calc(100vw+12rem)] min-h-screen md:min-h-auto md:w-[80%] md:aspect-[60/43] py-6 pt-22 px-12 md:px-auto overflow-x-hidden hide-scrollbar"
       >
-        <h2 className="text-[2.5rem] text-center text-white font-bold tracking-wider">
+        <h2 className="text-[20px] md:text-[2.5rem] text-center text-white font-bold tracking-wider">
           Takmičite se i osvajajte vrh!
         </h2>
-        <p className="text-white text-center opacity-60 mb-2 w-[55%] mx-auto">
+        <p className="text-[12px] md:text-[1rem] text-white text-center opacity-60 mb-2 w-[55%] mx-auto">
           Izazovite svoje prijatelje i igrače širom sveta. Pokažite da ste
           najbolji i zauzmite svoje mjesto na leaderboardu!
         </p>
         <Button
           onClick={openPopup}
-          className="bg-white text-[#2559D2] text-[1.25rem] font-bold rounded-[5px] py-3 px-20 cursor-pointer relative left-[50%] translate-x-[-50%] my-3 mt-8"
-        >Započni kviz</Button>
+          className="bg-white text-[#2559D2] text-[1rem] md:text-[1.25rem] font-bold rounded-[5px] py-3 px-16 md:px-20 cursor-pointer relative left-[50%] translate-x-[-50%] my-3 mt-8"
+        >
+          Započni kviz
+        </Button>
         <div className="flex flex-col items-center justify-center gap-3 mt-14">
           {tempTopScores.map((user) => (
             <ScoreCard
